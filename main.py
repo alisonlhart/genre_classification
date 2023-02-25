1357c4a7224365fb6f408a3bdf9abee9c1914350
Ali <alhart@redhat.com>
2023-02-24 23:24:14 -0500

    Pipeline code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import mlflow
 import os
+import logging
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
@@ -20,11 +23,12 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
-        steps_to_execute = config["main"]["execute_steps"]
+        steps_to_execute = list(config["main"]["execute_steps"])
+
 
     # Download step
     if "download" in steps_to_execute:
+        logger.info("Downloading and reading artifact")
 
         _ = mlflow.run(
             os.path.join(root_path, "download"),
@@ -38,21 +42,52 @@ def go(config: DictConfig):
         )
 
     if "preprocess" in steps_to_execute:
+        logger.info("Preprocessing data")
 
-        ## YOUR CODE HERE: call the preprocess step
+        _ = mlflow.run(
+            os.path.join(root_path, "preprocess"),
+            "main",
+            parameters={
+                "input_artifact": "raw_data.parquet:latest",
+                "artifact_name": "preprocessed_data.csv",
+                "artifact_type": "processed_data",
+                "artifact_description": "Data after processing",
+            },
+        )
         pass
 
     if "check_data" in steps_to_execute:
+        logger.info("Checking preprocessed data")
 
-        ## YOUR CODE HERE: call the check_data step
+        _ = mlflow.run(
+            os.path.join(root_path, "check_data"),
+            "main",
+            parameters={
+                "reference_artifact": config["data"]["reference_dataset"],
+                "sample_artifact":"preprocessed_data.csv:latest",
+                "ks_alpha": config["data"]["ks_alpha"],
+            },
+        )        
         pass
 
     if "segregate" in steps_to_execute:
-
-        ## YOUR CODE HERE: call the segregate step
+        logger.info("Stratifying data")
+  
+        _ = mlflow.run(
+            os.path.join(root_path, "segregate"),
+            "main",
+            parameters={
+                "input_artifact":"preprocessed_data.csv:latest",
+                "artifact_root":"dataset",
+                "artifact_type":"stratified_data",
+                "test_size":config["data"]["test_size"],
+                "stratify": config["data"]["stratify"],                
+            },       
+        )
         pass
 
     if "random_forest" in steps_to_execute:
+        logger.info("Random forest")
 
         # Serialize decision tree configuration
         model_config = os.path.abspath("random_forest_config.yml")
@@ -60,12 +95,32 @@ def go(config: DictConfig):
         with open(model_config, "w+") as fp:
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
-        ## YOUR CODE HERE: call the random_forest step
+
+        _ = mlflow.run(
+            os.path.join(root_path, "random_forest"),
+            "main",
+            parameters={
+                "train_data":"dataset_train.csv:latest",
+                "model_config": model_config,
+                "export_artifact": config["random_forest_pipeline"]["export_artifact"],
+                "random_seed": config["main"]["random_seed"],
+                "val_size": config["data"]["val_size"],
+                "stratify": config["data"]["stratify"],    
+            },
+        )
         pass
 
     if "evaluate" in steps_to_execute:
+        logger.info("Evaluating model")
 
-        ## YOUR CODE HERE: call the evaluate step
+        _ = mlflow.run(
+            os.path.join(root_path, "evaluate"),
+            "main",
+            parameters={
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                "test_data":"dataset_test.csv:latest",                
+            }
+        )
         pass
 
 
